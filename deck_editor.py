e8d48a7949fdca3953859308a529a07fce9d5f67
semperfi <mtavor556@gmail.com>
2026-03-09 23:31:47 +0200

    improved server

--- deck_editor.py
+++ deck_editor.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 """Entry point for the MTG deck editor web server."""
 
+import threading
+import webbrowser
+
 import uvicorn
 
 from src.deck_editor.app import app
 
+_HOST = "127.0.0.1"
+_PORT = 8000
+_URL = f"http://{_HOST}:{_PORT}"
+
+
+def _open_browser_after_delay() -> None:
+    threading.Timer(1.5, lambda: webbrowser.open(_URL)).start()
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    _open_browser_after_delay()
+    uvicorn.run(app, host=_HOST, port=_PORT)
